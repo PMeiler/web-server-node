31bb2fbfd0e269db374c0f1b9713392795167def
Patrick Meiler <patrick@meiler.org>
2018-02-27 20:13:23 +0100

    Added middleware!

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 const express = require("express")
 var app = express();
-
-
-var middleware = {
-	requestAuthentication: function(req,res,next){
-		console.log("Private Route taken!")
-		next();
-	},
-	logger: function(req,res,next){
-		console.log("Request: " + new Date().toString() +" "+ req.method +"  "+ req.originalUrl );
-		next();
-	}
-}
+var middleware = require("./middleware.js")
 
 app.use(middleware.logger);
 
